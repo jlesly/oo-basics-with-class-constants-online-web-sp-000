c80a7bb0065a99066a01a7db0a13f231a750ef3e
Lesly Juarez <leslyj2@gmail.com>
2020-07-22 19:55:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -18,3 +18,24 @@ class Shoe
     BRANDS << brand 
   end
 end
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
+  end
+
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+end
